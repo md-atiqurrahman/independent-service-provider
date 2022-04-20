0aef9c1e9fa1c3a130418cb925458f5f54c32e4a
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-04-20 22:57:05 +0600

    add banner

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css';
 import logo from '../../../images/Header/logo.png';
 
 
 const Header = () => {
     return (
-        <>
-            <Navbar className='navbar' sticky='top'>
-                <Container>
-                    <Navbar.Brand>
+        <Navbar bg="light" expand="lg" sticky='top'>
+            <Container className='nav-text-container' >
+                <div>
+                    <Navbar.Brand href="#home" className='logo-container'>
                         <img src={logo} alt="" />
+                        <h3>Catering By Ozdemir</h3>
                     </Navbar.Brand>
-                    <Navbar.Collapse className="link-names me-auto justify-content-end">
-                        <Nav.Link to="/home">Home</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#about">About me</Nav.Link>
-                        <Nav.Link href="#blogs">Blogs</Nav.Link>
-                        <Nav.Link href="#login">Login</Nav.Link>
+                </div>
+                <div>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto link-names">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#services">Services</Nav.Link>
+                            <Nav.Link href="#about">About me</Nav.Link>
+                            <Nav.Link href="#blogs">Blogs</Nav.Link>
+                            <Nav.Link href="#blogs">login</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
+                </div>
+            </Container>
+        </Navbar>
     );
 };
 
